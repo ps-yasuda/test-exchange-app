abbb85b8bfcdc47bde312b4df41d8a33d68d5841
yu-mizuno-himacs <yu-mizuno@himacs.co.jp>
2022-10-20 10:24:09 +0900

    test

--- test-exchange-app/src/App.js
+++ test-exchange-app/src/App.js
@@ -7,11 +7,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.sasasasasasasasaa
         </p>
         <a
           className="App-link"
-          test
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
