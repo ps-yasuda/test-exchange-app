8247499cba8a6d3ff60da8e46e397ccb5be3b6d2
yu-mizuno-himacs <yu-mizuno@himacs.co.jp>
2022-10-20 16:01:49 +0900

    test2

---
+++ test-exchange-app/src/ui-components/Mainmenu.jsx
@@ -0,0 +1,347 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import * as React from "react";
+import { getOverrideProps } from "@aws-amplify/ui-react/internal";
+import { Text, View } from "@aws-amplify/ui-react";
+export default function Mainmenu(props) {
+  const { overrides, ...rest } = props;
+  return (
+    <View
+      width="921px"
+      height="193px"
+      overflow="hidden"
+      position="relative"
+      padding="0px 0px 0px 0px"
+      backgroundColor="rgba(255,255,255,1)"
+      {...rest}
+      {...getOverrideProps(overrides, "Mainmenu")}
+    >
+      <Text
+        fontFamily="Inter"
+        fontSize="16px"
+        fontWeight="400"
+        color="rgba(0,0,0,1)"
+        lineHeight="24px"
+        textAlign="left"
+        display="flex"
+        direction="column"
+        justifyContent="flex-start"
+        width="243px"
+        height="29px"
+        position="absolute"
+        top="14px"
+        left="0px"
+        padding="0px 0px 0px 0px"
+        whiteSpace="pre-wrap"
+        children="取引システム　メインメニュー"
+        {...getOverrideProps(
+          overrides,
+          "\u53D6\u5F15\u30B7\u30B9\u30C6\u30E0\u3000\u30E1\u30A4\u30F3\u30E1\u30CB\u30E5\u30FC"
+        )}
+      ></Text>
+      <Text
+        fontFamily="Inter"
+        fontSize="16px"
+        fontWeight="400"
+        color="rgba(0,0,0,1)"
+        lineHeight="24px"
+        textAlign="left"
+        display="flex"
+        direction="column"
+        justifyContent="flex-start"
+        width="129px"
+        height="29px"
+        position="absolute"
+        top="14px"
+        left="463px"
+        padding="0px 0px 0px 0px"
+        whiteSpace="pre-wrap"
+        children="ログインユーザ："
+        {...getOverrideProps(
+          overrides,
+          "\u30ED\u30B0\u30A4\u30F3\u30E6\u30FC\u30B6\uFF1A"
+        )}
+      ></Text>
+      <Text
+        fontFamily="Inter"
+        fontSize="16px"
+        fontWeight="400"
+        color="rgba(0,0,0,1)"
+        lineHeight="24px"
+        textAlign="left"
+        display="flex"
+        direction="column"
+        justifyContent="flex-start"
+        width="129px"
+        height="29px"
+        position="absolute"
+        top="14px"
+        left="640px"
+        padding="0px 0px 0px 0px"
+        whiteSpace="pre-wrap"
+        children="○○"
+        {...getOverrideProps(overrides, "\u25CB\u25CB")}
+      ></Text>
+      <View
+        padding="0px 0px 0px 0px"
+        width="101px"
+        height="43px"
+        position="absolute"
+        top="7px"
+        left="769px"
+        {...getOverrideProps(overrides, "Group 29")}
+      >
+        <View
+          padding="0px 0px 0px 0px"
+          width="101px"
+          height="43px"
+          position="absolute"
+          top="0px"
+          left="0px"
+          {...getOverrideProps(overrides, "Group 2834462726")}
+        >
+          <View
+            width="101px"
+            height="43px"
+            position="absolute"
+            top="0px"
+            left="0px"
+            overflow="hidden"
+            padding="0px 0px 0px 0px"
+            backgroundColor="rgba(174,179,183,1)"
+            {...getOverrideProps(overrides, "Frame 41934462723")}
+          ></View>
+          <Text
+            fontFamily="Inter"
+            fontSize="16px"
+            fontWeight="400"
+            color="rgba(0,0,0,1)"
+            lineHeight="24px"
+            textAlign="center"
+            display="flex"
+            direction="column"
+            justifyContent="center"
+            width="95px"
+            height="43px"
+            position="absolute"
+            top="0px"
+            left="6px"
+            padding="0px 0px 0px 0px"
+            whiteSpace="pre-wrap"
+            children="ログアウト"
+            {...getOverrideProps(overrides, "\u30ED\u30B0\u30A2\u30A6\u30C8")}
+          ></Text>
+        </View>
+      </View>
+      <View
+        padding="0px 0px 0px 0px"
+        width="101px"
+        height="43px"
+        position="absolute"
+        top="82px"
+        left="223px"
+        {...getOverrideProps(overrides, "Group 30")}
+      >
+        <View
+          padding="0px 0px 0px 0px"
+          width="101px"
+          height="43px"
+          position="absolute"
+          top="0px"
+          left="0px"
+          {...getOverrideProps(overrides, "Group 2834462729")}
+        >
+          <View
+            width="101px"
+            height="43px"
+            position="absolute"
+            top="0px"
+            left="0px"
+            overflow="hidden"
+            padding="0px 0px 0px 0px"
+            backgroundColor="rgba(174,179,183,1)"
+            {...getOverrideProps(overrides, "Frame 41934462730")}
+          ></View>
+          <Text
+            fontFamily="Inter"
+            fontSize="16px"
+            fontWeight="400"
+            color="rgba(0,0,0,1)"
+            lineHeight="24px"
+            textAlign="center"
+            display="flex"
+            direction="column"
+            justifyContent="center"
+            width="101px"
+            height="43px"
+            position="absolute"
+            top="0px"
+            left="0px"
+            padding="0px 0px 0px 0px"
+            whiteSpace="pre-wrap"
+            children="ステータス"
+            {...getOverrideProps(overrides, "\u30B9\u30C6\u30FC\u30BF\u30B9")}
+          ></Text>
+        </View>
+      </View>
+      <View
+        padding="0px 0px 0px 0px"
+        width="101px"
+        height="43px"
+        position="absolute"
+        top="82px"
+        left="373px"
+        {...getOverrideProps(overrides, "Group 31")}
+      >
+        <View
+          padding="0px 0px 0px 0px"
+          width="101px"
+          height="43px"
+          position="absolute"
+          top="0px"
+          left="0px"
+          {...getOverrideProps(overrides, "Group 2834462733")}
+        >
+          <View
+            width="101px"
+            height="43px"
+            position="absolute"
+            top="0px"
+            left="0px"
+            overflow="hidden"
+            padding="0px 0px 0px 0px"
+            backgroundColor="rgba(174,179,183,1)"
+            {...getOverrideProps(overrides, "Frame 41934462734")}
+          ></View>
+          <Text
+            fontFamily="Inter"
+            fontSize="16px"
+            fontWeight="400"
+            color="rgba(0,0,0,1)"
+            lineHeight="24px"
+            textAlign="center"
+            display="flex"
+            direction="column"
+            justifyContent="center"
+            width="95px"
+            height="43px"
+            position="absolute"
+            top="0px"
+            left="6px"
+            padding="0px 0px 0px 0px"
+            whiteSpace="pre-wrap"
+            children="商品情報"
+            {...getOverrideProps(overrides, "\u5546\u54C1\u60C5\u5831")}
+          ></Text>
+        </View>
+      </View>
+      <View
+        padding="0px 0px 0px 0px"
+        width="101px"
+        height="43px"
+        position="absolute"
+        top="82px"
+        left="523px"
+        {...getOverrideProps(overrides, "Group 32")}
+      >
+        <View
+          padding="0px 0px 0px 0px"
+          width="101px"
+          height="43px"
+          position="absolute"
+          top="0px"
+          left="0px"
+          {...getOverrideProps(overrides, "Group 2834462737")}
+        >
+          <View
+            width="101px"
+            height="43px"
+            position="absolute"
+            top="0px"
+            left="0px"
+            overflow="hidden"
+            padding="0px 0px 0px 0px"
+            backgroundColor="rgba(174,179,183,1)"
+            {...getOverrideProps(overrides, "Frame 41934462738")}
+          ></View>
+          <Text
+            fontFamily="Inter"
+            fontSize="16px"
+            fontWeight="400"
+            color="rgba(0,0,0,1)"
+            lineHeight="24px"
+            textAlign="center"
+            display="flex"
+            direction="column"
+            justifyContent="center"
+            width="101px"
+            height="43px"
+            position="absolute"
+            top="0px"
+            left="0px"
+            padding="0px 0px 0px 0px"
+            whiteSpace="pre-wrap"
+            children="参加者情報"
+            {...getOverrideProps(overrides, "\u53C2\u52A0\u8005\u60C5\u5831")}
+          ></Text>
+        </View>
+      </View>
+      <View
+        padding="0px 0px 0px 0px"
+        width="101px"
+        height="43px"
+        position="absolute"
+        top="82px"
+        left="673px"
+        {...getOverrideProps(overrides, "Group 33")}
+      >
+        <View
+          padding="0px 0px 0px 0px"
+          width="101px"
+          height="43px"
+          position="absolute"
+          top="0px"
+          left="0px"
+          {...getOverrideProps(overrides, "Group 2834462741")}
+        >
+          <View
+            width="101px"
+            height="43px"
+            position="absolute"
+            top="0px"
+            left="0px"
+            overflow="hidden"
+            padding="0px 0px 0px 0px"
+            backgroundColor="rgba(174,179,183,1)"
+            {...getOverrideProps(overrides, "Frame 41934462742")}
+          ></View>
+          <Text
+            fontFamily="Inter"
+            fontSize="16px"
+            fontWeight="400"
+            color="rgba(0,0,0,1)"
+            lineHeight="24px"
+            textAlign="center"
+            display="flex"
+            direction="column"
+            justifyContent="center"
+            width="101px"
+            height="43px"
+            position="absolute"
+            top="0px"
+            left="0px"
+            padding="0px 0px 0px 0px"
+            whiteSpace="pre-wrap"
+            children="取引監視"
+            {...getOverrideProps(overrides, "\u53D6\u5F15\u76E3\u8996")}
+          ></Text>
+        </View>
+      </View>
+    </View>
+  );
+}
